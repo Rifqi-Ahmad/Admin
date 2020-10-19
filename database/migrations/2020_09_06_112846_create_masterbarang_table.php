9a0f2d75490d2a84aa171ab709e8d7e1bd25a6dd
Rifqi-Ahmad <rifqi.alhuwaidi@gmail.com>
2020-10-19 20:06:45 +0700

    sasdas

--- database/migrations/2020_09_06_112846_create_masterbarang_table.php
+++ database/migrations/2020_09_06_112846_create_masterbarang_table.php
@@ -16,7 +16,7 @@ class CreateMasterbarangTable extends Migration
     {
 
         Schema::create('masterbarang', function (Blueprint $table) {
-            $table->increments('id')->from($integer);
+            $table->increments('id')->from(0);
             $table->char('code', 6)->unique();
             $table->string('desc');
             $table->string('color');
